34f552555b26b37bee1a70c95e2ce0310292880c
Yuri140 <yurisousa.carneiro2006@gmail.com>
2023-08-31 21:53:31 -0300

    tela nova

---
+++ app/src/main/java/br/com/etecia/myapp/CabrasMachosActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CabrasMachosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cabras_machos_layout);
+    }
+}
